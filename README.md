f13c67c6028c0b00ddf1f1302b9f369c767889e5
Amisha PRIYA <priyaamisha03@gmail.com>
2025-04-18 01:25:45 +0530

    Add theme label + update Readme file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ThemeTogglerCSS
 Vanilla JS and CSS based theme toggler UI
+
+# Points:
+1. Label
+2. Toggle switch
+3. this in arrow functions
+4. varibales in :root
+5. ::before
+6. JS
+
+# Light theme:
+
+![Light theme](image-2.png)
+
+# Dark theme:
+
+![Dark theme](image-1.png)
--- index.js
+++ index.js
@@ -4,8 +4,16 @@ document.documentElement.setAttribute('data-theme', currentTheme);
 const themeToggleCheckbox = document.getElementById('theme-checkbox');
 themeToggleCheckbox.checked = currentTheme === 'dark';
 
+const themeLabel = document.getElementById('theme-label');
+setThemeLabel(currentTheme);
+
 themeToggleCheckbox.addEventListener('change', function () {
-    const selectedTheme = this.checked ? 'dark' : 'light'; // this binds to the DOM object themeToggleCheckbox
+    const selectedTheme = this.checked ? 'dark' : 'light'; // this binds to the DOM object themeToggleCheckbox, undefined when used in arrow functions.
     sessionStorage.setItem('theme', selectedTheme);
     document.documentElement.setAttribute('data-theme', selectedTheme);
+    setThemeLabel(selectedTheme);
 });
+
+function setThemeLabel(theme) {
+    themeLabel.textContent = theme === 'dark' ? 'Dark Mode' : 'Light Mode';
+}
